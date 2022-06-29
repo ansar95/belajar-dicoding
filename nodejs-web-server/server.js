aa7327bbd37c88554e5240c1f8b28293daf60b26
ansar sattu <ansarthelingu95@gmail.com>
2022-06-29 11:30:55 +0800

    body request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,17 +1,42 @@
 const http = require ('http');
 
 const requestListener = (request, response) => {
+response.setHeader('Content-Type', 'text/html');
+response.statusCode = 200;
 
-  response.setHeader('Content-Type', 'text/html');
- 
-  response.statusCode = 200;
-  response.end('<h1>Halo HTTP Server!</h1>');
+  const {method} = request;
 
+  if(method === 'GET'){
+    response.end('<h1>Hello GET</h1>')
+  }
 
+  if(method === 'POST'){
+    let body = [];
+    
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    
+    request.on('end', () => {
+      body = Buffer.concat(body).toString();
+      const { name } = JSON.parse(body);
+      response.end(`<h1>Hallo, ${name}! </h1>`);
+    });  
+  }
+
+  if(method === 'PUT'){
+    response.end('<h1>Hello PUT</h1>')
+  }
+
+  if(method === 'DELETE'){
+    response.end('<h1>Hello DELETE</h1>')
+  }
+
+  
 };
 
-const server = http.createServer(requestListener);
 
+const server = http.createServer(requestListener);
 
 const port = 5000;
 const host = 'localhost';
