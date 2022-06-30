efcfaad3c9604e30386491647d873df8e52cd129
ansar sattu <ansarthelingu95@gmail.com>
2022-06-30 13:04:32 +0800

    response status

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -2,19 +2,48 @@ const http = require ('http');
 
 const requestListener = (request, response) => {
 response.setHeader('Content-Type', 'text/html');
-response.statusCode = 200;
 
-  
 
-  const {method, url} = request;
-  
-  if (url === '/'){
 
-    //curl http://localhost:5000/
+const {method, url} = request;
+
+if (url === '/'){
+  if(method === 'GET'){
+      response.statusCode = 200;
+      response.end('<h1>Ini Halaman Homepage</h1>');
+    }else{
+      response.statusCode = 400;
+      response.end(`<h1>Halaman tidak dapat di akses dengan ${method} request</h1>`);
+    }
+    
   }else if (url === '/about'){
+    if(method === 'GET'){
+      response.statusCode = 200;
+      response.end('<h1>Ini Halaman About</h1>');
+    }else if(method === 'POST'){
+      let body = [];
 
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+      response.statusCode = 200;
+        response.end(`<h1>Hallo, ${name} ini halaman about </h1>`);
+      });  
+    
+
+    }else{
+      response.statusCode = 400;
+
+      response.end(`<h1>Halaman tidak dapat di akses dengan ${method} request</h1>`);
+    }
 
   }else{
+    response.statusCode = 404;
+
     response.end ('<h1>Halaman Tidak Di Temukan</h1>');
   }
     
